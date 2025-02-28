75ad6218a306c73bf1862c010ddce937bcb68b83
dcifligu <deivi.cifligu@gmail.com>
2025-02-28 16:34:06 +0100

    NavBar creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def hide_nav?
+    devise_pages_without_nav = [
+      { controller: "registrations", action: "new" },
+      { controller: "registrations", action: "create" },
+      { controller: "sessions", action: "new" },
+      { controller: "sessions", action: "create" }
+      # Add other controller/action pairs as needed
+    ]
+
+    devise_pages_without_nav.any? do |page|
+      controller_name == page[:controller] && action_name == page[:action]
+    end
+  end
 end
